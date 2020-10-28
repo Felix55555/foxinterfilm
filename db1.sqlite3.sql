f00ce37eee0698d7b37ec0d3fa1b0b9939c249d3
bitcoinmonster23 <39277307+bitcoinmonster23@users.noreply.github.com>
2020-10-28 16:06:35 +0200

    Add files via upload

---
+++ db1.sqlite3.sql
@@ -0,0 +1,299 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/REC-html40/strict.dtd">
+<html>
+    <meta http-equiv="Content-Type" content="text/html; charset=System"/>
+    <title>Экспортированная база данных: mydatabase</title>
+    <style type="text/css">
+        table
+        {
+            border-style: solid;
+            border-width: 1px;
+            border-color: black;
+            border-collapse: collapse;
+        }
+        
+        table tr
+        {
+            background-color: white;
+        }
+        
+        table tr.header
+        {
+            background-color: #DDDDDD;
+        }
+        
+        table tr.title
+        {
+            background-color: #EEEEEE;
+        }
+        
+        table tr td
+        {
+            padding: 0px 3px 0px 3px;
+            border-style: solid;
+            border-width: 1px;
+            border-color: #666666;
+        }
+        
+        table tr td.null
+        {
+            color: #999999;
+            text-align: center;
+            padding: 0px 3px 0px 3px;
+            border-style: solid;
+            border-width: 1px;
+            border-color: #666666;
+        }
+        
+        table tr td.separator
+        {
+            padding: 0px 3px 0px 3px;
+            border-style: solid;
+            border-width: 1px;
+            border-color: #666666;
+            background-color: #DDDDDD;
+        }
+        
+        table tr td.rownum
+        {
+            padding: 0px 3px 0px 3px;
+            border-style: solid;
+            border-width: 1px;
+            border-color: #666666;
+            background-color: #DDDDDD;
+            text-align: right;
+        }
+        
+    </style>
+    <body>
+        <table>
+            <tr class="title">
+                <td colspan="7" align="center">Таблица: Members</td>
+            </tr>
+            <tr class="header">
+                <td align="right">
+                    <b><i>#</i></b>
+                </td>
+                <td>
+                    <b>idN</b><br/>int
+                </td>
+                <td>
+                    <b>usernames</b><br/>varchar
+                </td>
+                <td>
+                    <b>userid</b><br/>int
+                </td>
+                <td>
+                    <b>created_at</b><br/>timestamp
+                </td>
+                <td>
+                    <b>imya</b><br/>varchar
+                </td>
+                <td>
+                    <b>famil</b><br/>varchar
+                </td>
+            </tr>
+        </table>
+        <br/><br/>
+        <table>
+            <tr class="title">
+                <td colspan="7" align="center">Таблица: Members2</td>
+            </tr>
+            <tr class="header">
+                <td align="right">
+                    <b><i>#</i></b>
+                </td>
+                <td>
+                    <b>idN</b><br/>int
+                </td>
+                <td>
+                    <b>usernames</b><br/>varchar
+                </td>
+                <td>
+                    <b>userid</b><br/>int
+                </td>
+                <td>
+                    <b>created_at</b><br/>timestamp
+                </td>
+                <td>
+                    <b>imya</b><br/>varchar
+                </td>
+                <td>
+                    <b>famil</b><br/>varchar
+                </td>
+            </tr>
+        </table>
+        <br/><br/>
+        <table>
+            <tr class="title">
+                <td colspan="8" align="center">Таблица: Poisk</td>
+            </tr>
+            <tr class="header">
+                <td align="right">
+                    <b><i>#</i></b>
+                </td>
+                <td>
+                    <b>id</b><br/>int
+                </td>
+                <td>
+                    <b>usernames</b><br/>varchar
+                </td>
+                <td>
+                    <b>userid</b><br/>int
+                </td>
+                <td>
+                    <b>created_at</b><br/>timestamp
+                </td>
+                <td>
+                    <b>zapros</b><br/>varchar
+                </td>
+                <td>
+                    <b>imya</b><br/>varchar
+                </td>
+                <td>
+                    <b>famil</b><br/>varchar
+                </td>
+            </tr>
+        </table>
+        <br/><br/>
+        <table>
+            <tr class="title">
+                <td colspan="2" align="center">Таблица: test_timestamp</td>
+            </tr>
+            <tr class="header">
+                <td align="right">
+                    <b><i>#</i></b>
+                </td>
+                <td>
+                    <b>t1</b><br/>timestamp
+                </td>
+            </tr>
+        </table>
+        <br/><br/>
+        <table>
+            <tr class="title">
+                <td colspan="5" align="center">Таблица: Users</td>
+            </tr>
+            <tr class="header">
+                <td align="right">
+                    <b><i>#</i></b>
+                </td>
+                <td>
+                    <b>usernames</b><br/>varchar
+                </td>
+                <td>
+                    <b>userIds</b><br/>int
+                </td>
+                <td>
+                    <b>imya</b><br/>varchar
+                </td>
+                <td>
+                    <b>famil</b><br/>varchar
+                </td>
+            </tr>
+            <tr>
+                <td class="rownum">
+                    <i>1</i>
+                </td>
+                <td align="left">
+                    test
+                </td>
+                <td align="right">
+                    324969393
+                </td>
+                <td align="left" class="null">
+                    <i>NULL</i>
+                </td>
+                <td align="left" class="null">
+                    <i>NULL</i>
+                </td>
+            </tr>
+            <tr>
+                <td class="rownum">
+                    <i>2</i>
+                </td>
+                <td align="left">
+                    screlizer
+                </td>
+                <td align="right">
+                    324969393
+                </td>
+                <td align="left" class="null">
+                    <i>NULL</i>
+                </td>
+                <td align="left" class="null">
+                    <i>NULL</i>
+                </td>
+            </tr>
+            <tr>
+                <td class="rownum">
+                    <i>3</i>
+                </td>
+                <td align="left">
+                    Andme
+                </td>
+                <td align="right">
+                    324969393
+                </td>
+                <td align="left" class="null">
+                    <i>NULL</i>
+                </td>
+                <td align="left" class="null">
+                    <i>NULL</i>
+                </td>
+            </tr>
+            <tr>
+                <td class="rownum">
+                    <i>4</i>
+                </td>
+                <td align="left">
+                    Kontakt
+                </td>
+                <td align="right">
+                    324969393
+                </td>
+                <td align="left" class="null">
+                    <i>NULL</i>
+                </td>
+                <td align="left" class="null">
+                    <i>NULL</i>
+                </td>
+            </tr>
+            <tr>
+                <td class="rownum">
+                    <i>5</i>
+                </td>
+                <td align="left">
+                    xTube
+                </td>
+                <td align="right">
+                    324969393
+                </td>
+                <td align="left" class="null">
+                    <i>NULL</i>
+                </td>
+                <td align="left" class="null">
+                    <i>NULL</i>
+                </td>
+            </tr>
+            <tr>
+                <td class="rownum">
+                    <i>6</i>
+                </td>
+                <td align="left">
+                    None
+                </td>
+                <td align="right">
+                    324969393
+                </td>
+                <td align="left" class="null">
+                    <i>NULL</i>
+                </td>
+                <td align="left" class="null">
+                    <i>NULL</i>
+                </td>
+            </tr>
+        </table>
+        <br/><br/>
+        <i>Документ сгенерирован с помощью SQLiteStudio v3.2.1 в ср окт. 28 16:06:04 2020</i>
+    </body>
+</html>
